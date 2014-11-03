3a97a2ff4a2ba26594ba4753d6f5bc8aaf30cce9
Doug Estey <doug@tailoredux.com>
2014-11-03 11:06:27 -0500

    cleanup

--- src/js/_search.js
+++ src/js/_search.js
@@ -1,43 +1,22 @@
 var statusEl = document.getElementById("status");
 
-function onSuggestionsRequested(e)
-  {
-    var promise = FlickrSearchLibrary.FlickrSearcher.getHotTagsAsync(e.detail.queryText);
-
-    promise.done(
-      function (results)
-      {
-        e.detail.searchSuggestionCollection.appendQuerySuggestions(results);
-      }
-    );
-    e.detail.setPromise(promise);
-  }
-
 function suggestionsRequestedHandler(args) {
-
     var query = args.detail.queryText.toLocaleLowerCase();
-
     var promise = services.searchProducts(query);
-
     promise.done(function(){
         // retrieve the system-supplied suggestions (i.e. previous searches performed by this user)
         var suggestionCollection = args.detail.searchSuggestionCollection;
-
         // check that the query is at least one character & that we have data from the API
         if (query.length > 0 && data.searchResults) {
-            
             data.searchResults.forEach(
                 function (element, index, array) {
                     if (element.name.substr(0, query.length).toLocaleLowerCase() === query) {
                         suggestionCollection.appendQuerySuggestion(element.name);
                     }
                 });
-
         }
     });
-
     args.detail.setPromise(promise);
-
 }
 
 function querySubmittedHandler(eventObject) {
@@ -45,9 +24,27 @@ function querySubmittedHandler(eventObject) {
     WinJS.log && WinJS.log(queryText, "sample", "status");
 }
 
+// example
+/*
+function onQuerySubmitted(e)
+  {
+    var queryText = e.detail.queryText;
+
+    var promise = FlickrSearchLibrary.FlickrSearcher.searchAsync(queryText);
+
+    promise.done(
+      function (results)
+      {
+        var bindingList = new WinJS.Binding.List(results);
+        var listControl = document.getElementById('listView').winControl;
+        listControl.itemDataSource = bindingList.dataSource;
+      }
+    );
+  }
+*/
+
 WinJS.log = function (msg, source, type) {
     if (type === "status") {
         statusEl.innerHTML += msg + "<br/>";
     }
 }
-
