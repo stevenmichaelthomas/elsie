523693ec23339cb77a1a31f174f9b36e50a3eb27
Doug Estey <doug@tailoredux.com>
2014-11-08 16:47:07 -0500

    back button support

--- src/js/_ui.js
+++ src/js/_ui.js
@@ -4,4 +4,14 @@ ui.init = function(){
 	WinJS.UI.processAll().then(function(){
 		WinJS.Navigation.navigate('./home.html');
 	});
+  window.addEventListener('keyup', backButtonGlobalKeyUpHandler, false)
+  var KEY_LEFT = "Left";
+  var KEY_BROWSER_BACK = "BrowserBack";
+  var MOUSE_BACK_BUTTON = 3;
+  function backButtonGlobalKeyUpHandler(event) {
+        // Navigates back when (alt + left) or BrowserBack keys are released.
+        if ((event.key === KEY_LEFT && event.altKey && !event.shiftKey && !event.ctrlKey) || (event.key === KEY_BROWSER_BACK)) {
+            nav.back();
+        }
+    }
 }
