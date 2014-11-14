715c70110740f5564d3f9bfea97f1e9c9e1cfd08
Doug Estey <doug@tailoredux.com>
2014-11-14 13:29:07 -0500

    if winRT is available, hide statusbar

--- src/js/_ui.js
+++ src/js/_ui.js
@@ -11,6 +11,11 @@
         WinJS.Navigation.back();
       }*/
 
+      // if we have access to the Windows Runtime, show the statusbar
+      if (typeof Windows !== 'undefined') {
+        Windows.UI.ViewManagement.StatusBar.getForCurrentView().showAsync();
+      }
+
     	WinJS.UI.processAll().then(function(){
     		WinJS.Navigation.navigate('./home.html');
     	});
