a76461bd3ecc0494ffc00ef5f04531d7fb12842a
Doug Estey <doug@tailoredux.com>
2015-04-29 11:31:57 -0400

    add location service

---
+++ src/js/services/location.js
@@ -0,0 +1,46 @@
+angular.module('elsie.services')
+.factory('Location', function($q) {
+
+  var location = {};
+  var _sync = function(){
+    localStorage["Elsie_cachedLocation"] = JSON.stringify(location);
+  };
+      
+  return {
+    initialize: function(){
+      if (!localStorage["Elsie_cachedLocation"]){
+        location = {
+          latitude: "43.656025",
+          longitude: "-79.380257"
+        };
+        localStorage["Elsie_cachedLocation"] = JSON.stringify(location);
+      } else {
+        location = JSON.parse(localStorage["Elsie_cachedLocation"]);
+      }
+    },
+    refresh: function(){
+      var deferred = $q.defer();
+      //Elsie.Interface.showLoadingAnimation("Getting your location...");
+      var onSuccess = function(position) {
+        //Elsie.Interface.hideLoadingAnimation();
+        var newLocation = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        };
+        location = newLocation;
+        _sync();
+        deferred.resolve();
+      };
+      var onError = function() {
+        //Elsie.Interface.hideLoadingAnimation();
+        var text = "Elsie couldn't get your location. She won't be able to provide you with accurate information until she has it.";
+        var link = { label: "Retry", action: "getLocation" };
+        //Elsie.Interface.showLocationError(text, link);
+        deferred.resolve();
+      };
+      navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 6500 });
+      return deferred.promise;
+    }
+  }
+
+})
