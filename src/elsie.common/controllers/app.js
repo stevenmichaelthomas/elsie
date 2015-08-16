b11158b98d2540c305a89330250f8fd0806aa760
Doug Estey <doug@designcofounders.com>
2015-08-16 14:42:45 -0700

    add runtime block

--- src/elsie.common/controllers/app.js
+++ src/elsie.common/controllers/app.js
@@ -1,12 +1,30 @@
 angular.module('elsie.common', [])
 .controller('AppCtrl', function($scope, $state, $mdSidenav){
+  
   function buildToggler(navID) {
     return function() {
       return $mdSidenav(navID).toggle();
     }
   }
+
   $scope.toggleMenu = buildToggler('menu'); 
   $scope.go = function(destination){
     $state.go(destination);
   }
+  $scope.followLink = function(url){
+    //navigator.app.loadUrl(url, {openExternal : true});
+    window.open(url, '_system');
+  };
+
+  (function(){
+    //init routine
+    if (typeof Windows !== 'undefined') {
+      // WinRT is available
+      var statusBar = Windows.UI.ViewManagement.StatusBar.getForCurrentView();
+      statusBar.backgroundColor = {a: 255, r: 103, g: 65, b: 114};
+      statusBar.backgroundOpacity = 1;
+      statusBar.showAsync();
+    }
+  }()
+  
 });
