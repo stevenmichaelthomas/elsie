9269c8da50fea7da87e74906d141e53e0bfd92ce
Doug Estey <doug@designcofounders.com>
2015-08-17 22:22:13 -0400

    implement filters file; add percentage filter

---
+++ src/elsie/filters.js
@@ -0,0 +1,6 @@
+angular.module('elsie')
+.filter('percentage', ['$filter', function ($filter) {
+  return function (input, decimals) {
+    return $filter('number')(input * 100, decimals) + '%';
+  };
+}]);
