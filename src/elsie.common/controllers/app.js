5e800c2073bdc135768e66e7968e88f85f097165
Doug Estey <doug@designcofounders.com>
2015-08-26 09:13:14 -0400

    animation overhaul with inspiration from ionic

--- src/elsie.common/controllers/app.js
+++ src/elsie.common/controllers/app.js
@@ -1,15 +1,50 @@
 angular.module('elsie.common')
-.controller('AppCtrl', function($scope, $state, $history, Actions, Navigator){
+.controller('AppCtrl', function($scope, $state, $history, $timeout, Actions, Navigator){
+
+  var transitionEnd = function(){
+    var el = document.querySelector('#main');
+    var classes = el.classList;
+    for (var c = classes.length; c > 0; c--){
+      if (classes[c]){
+        var arr = classes[c].split('-');
+        var index = -1;
+        if (arr.length > 0){
+          index = arr.indexOf('direction');
+          if (index !== -1){
+            angular.element(el).removeClass(classes[c]);
+          }
+        }
+      }
+    }
+  };
+
+  var transitionStart = function(event, toState, toParams, fromState, fromParams){
+    var el = document.querySelector('#main');
+    if (toState.priority > fromState.priority){
+      angular.element(el).addClass('direction-forward');
+    } 
+    if (toState.priority < fromState.priority) {
+      angular.element(el).addClass('direction-back');
+    }
+    $timeout(function(){
+      transitionEnd();
+    }, 750);
+  };
+
   $scope.go = function(destination){
     Navigator.go(destination);
   };
+  
   $scope.followLink = function(url){
     //navigator.app.loadUrl(url, {openExternal : true});
     window.open(url, '_system');
   };
+  
+  $scope.$on('$stateChangeStart', transitionStart);
   $scope.barTransparency = Actions.transparent;
   $scope.barItems = Actions.get;
   $scope.barSearch = Actions.search;
+  
   (function(){
     //init routine
   })();
--- src/elsie.common/directives/stateClass.js
+++ src/elsie.common/directives/stateClass.js
@@ -20,38 +20,9 @@ angular.module('elsie.common')
         }
         var stateName = $state.current.name || 'init',
         normalizedStateName = 'state-' + stateName.replace(/\./g, '-');
-        if (lastState()){
-          if (lastState().priority < $state.current.priority){
-            $element.addClass('state-higher');
-          }
-          if (lastState().name === 'search' && $state.current.name === 'home'){
-            $element.addClass('no-animate');
-          }
-        }
-        if ($state.current.name === 'search'){
-          $element.addClass('no-animate');
-        }
         $element.addClass(normalizedStateName);
       };
-      var dealWithOldState = function(event, toState, toParams, fromState, fromParams){
-        if (toState.priority > $state.current.priority){
-          var classes = $element[0].classList;
-          for (var c = classes.length; c > 0; c--){
-            if (classes[c]){
-              var arr = classes[c].split('-');
-              var index = -1;
-              if (arr.length > 0){
-                index = arr.indexOf('state-higher');
-                if (index !== -1){
-                  $element.removeClass(classes[c]);
-                }
-              }
-            }
-          }
-        }
-      };
       $scope.$on('$viewContentLoaded', updateState);
-      $scope.$on('$stateChangeStart', dealWithOldState);
     }
   };
 }]);
