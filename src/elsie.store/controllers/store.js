70f099007e4424ff0b61fafcc0cf84771c5d4293
Doug Estey <doug@designcofounders.com>
2015-08-25 20:37:01 -0400

    improvements to stores

--- src/elsie.store/controllers/store.js
+++ src/elsie.store/controllers/store.js
@@ -1,6 +1,9 @@
 angular.module('elsie.store')
-.controller('StoreCtrl', function ($scope, $state, Stores, Map) {
+.controller('StoreCtrl', function ($scope, $state, Stores, Map, Actions) {
   (function(){
+    Actions.transparent(true);
+    Actions.search(false);
+    Actions.set({ menu: false, back: true });
     $scope.store = Stores.selected();
     $scope.image = {
       'background-image': 'url(' + Map.get($scope.store.latitude, $scope.store.longitude) + ')'
