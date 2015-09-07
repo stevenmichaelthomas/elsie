3abbb15a3038ab81872f88a063164e551641425a
Doug Estey <doug@designcofounders.com>
2015-09-07 18:34:21 -0400

    cache kwaf picks

--- src/elsie.product/services/picks.js
+++ src/elsie.product/services/picks.js
@@ -1,5 +1,5 @@
 angular.module('elsie.product')
-.factory('Picks', function($http, ELSIEAPI, Locator, Dialog, Scheduler) {
+.factory('Picks', function($http, $q, ELSIEAPI, Locator, Dialog, Scheduler) {
 
   var url = function() {
     return ELSIEAPI;
@@ -12,21 +12,26 @@ angular.module('elsie.product')
       
   return {
     latest: function(){
-      var req = url() + '/picks?sort=id%20DESC';
-      var process = $http.get(req).then(function(result){
-        if (result.status === 200){
-          cache.picks = result.data;
-          angular.forEach(cache.picks, function(p,i){
-            cache.picksHash[p.productNumber] = p;
-          });
-          return cache.picks;
-        } else {
-          Dialog.showConnectionError();
-          return [];
-        }
-      });
-      Scheduler.queue(process);
-      return process;
+      var deferred = $q.defer();
+      if (cache.picks.length === 0){
+        var req = url() + '/picks?sort=id%20DESC';
+        var process = $http.get(req).then(function(result){
+          if (result.status === 200){
+            cache.picks = result.data;
+            angular.forEach(cache.picks, function(p,i){
+              cache.picksHash[p.productNumber] = p;
+            });
+            deferred.resolve(cache.picks);
+          } else {
+            Dialog.showConnectionError();
+            return [];
+          }
+        });
+        Scheduler.queue(process);
+      } else {
+        deferred.resolve(cache.picks);
+      }
+      return deferred.promise;
     },
     one: function(){
       return cache.picks[0];
